a06b0bcc67bbe601a674c2a090f0f28c9cc5bd32
Aleika <aleikaalves@hotmail.com>
2018-10-01 07:26:00 -0300

    Update flipFlopJK.vhd

--- flipFlopJK.vhd
+++ flipFlopJK.vhd
@@ -4,32 +4,30 @@ use ieee.std_logic_1164.all;
 entity flipFlopJK is
 port(
 	J,K : in std_logic;
-	reset, enable : in std_logic;
+	clear, preset : in std_logic;
 	clk : in std_logic;
 	Q, Qbar: out std_logic
 );
 end flipFlopJK;
 
 architecture flipFlopArch of flipFlopJK is
-signal qsignal : std_logic;
+signal qsignal : std_logic:='0';
 begin
-	process(clk, reset, enable)
+	process(clk, clear, preset)
 	begin
-	if(clk'event and clk = '0') then 
-		if(reset = '1' and enable = '0') then
+		if(clear = '0') then
 			qsignal <= '0';
-		elsif(enable = '1' and reset = '0')	then
+		elsif(preset = '0')	then
+			qsignal <= '1';
+		elsif(clk'event and clk = '0') then 
 			if(J = '0' and K = '1') then
 				qsignal <= '1';
 			elsif(J = '1' and K = '0') then
 				qsignal <= '0';
 			elsif(J = '0' and K='0') then
 				qsignal <= not qsignal;
-			elsif(J = '1' and K='1') then
-				qsignal <= qsignal;
 			end if;
 		end if;
-	end if;
 	end process;
 	Q <= qsignal;
 	Qbar <= not qsignal;

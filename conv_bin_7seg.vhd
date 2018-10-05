eee643d62001f472a9657067f861e3bdd4876346
Aleika <aleikaalves@hotmail.com>
2018-10-05 10:02:46 -0300

    Update conv_bin_7seg.vhd

--- conv_bin_7seg.vhd
+++ conv_bin_7seg.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
  
 entity conv_bin_7seg is
-	Port ( B3,B2,B1,B0 : in STD_LOGIC;
+	Port ( a,b,c : in STD_LOGIC;
 	display: out STD_LOGIC_vector(6 downto 0));
 end conv_bin_7seg;
  
@@ -10,12 +10,12 @@ architecture Behavioral of conv_bin_7seg is
  
 begin
  
-display(6) <= not(B0 OR B2 OR (B1 AND B3) OR (NOT B1 AND NOT B3));
-display(5) <= not ((NOT B1) OR (NOT B2 AND NOT B3) OR (B2 AND B3));
-display(4) <= not (B1 OR NOT B2 OR B3);
-display(3) <= not ((NOT B1 AND NOT B3) OR (B2 AND NOT B3) OR (B1 AND NOT B2 AND B3) OR (NOT B1 AND B2) OR B0);
-display(2) <= not ((NOT B1 AND NOT B3) OR (B2 AND NOT B3));
-display(1) <= not (B0 OR (NOT B2 AND NOT B3) OR (B1 AND NOT B2) OR (B1 AND NOT B3));
-display(0) <= not (B0 OR (B1 AND NOT B2) OR ( NOT B1 AND B2) OR (B2 AND NOT B3));
+display(6) <= (a and (not b) and (not c)) or ((not a) and (not b) and c);
+display(5) <= (a and (not b) and c) or (a and b and (not c));
+display(4) <= ((not a)and b and (not c));
+display(3) <= ((not a) and (not b) and c) or (a and (not b) and (not c)) or (a and b and c);
+display(2) <= c or (a and (not b));
+display(1) <= ((not a ) and c) or ((not a) and b) or (b and c);
+display(0) <= ((not a ) and (not b)) or (a and b and c);
  
 end Behavioral;
